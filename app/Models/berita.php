f3bce1c17c2851928e659806cf346ce16c99d8a3
ridho140905 <ridho24si@mahasiswa.pcr.ac.id>
2025-11-16 00:12:39 +0700

    PUbah

--- app/Models/berita.php
+++ app/Models/berita.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class berita extends Model
+class Berita extends Model
 {
     use HasFactory;
 
