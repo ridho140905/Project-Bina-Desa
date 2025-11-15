336df6dbf5a26dc79ebe4c3ecb61d9504b906fe6
ridho140905 <ridho24si@mahasiswa.pcr.ac.id>
2025-11-15 23:06:52 +0700

    perbaikan deploy

--- app/Http/Controllers/BeritaController.php
+++ app/Http/Controllers/BeritaController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Berita;
+
+use Illuminate\Http\Request;
 use App\Models\KategoriBerita;
 
 class BeritaController extends Controller
